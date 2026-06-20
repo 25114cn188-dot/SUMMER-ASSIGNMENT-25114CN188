e6a1ccc0baa395e2b59a206b9267ecd1d35514f2
25114cn188-dot <25114cn188@glbitm.ac.in>
2026-06-20 07:53:01 +0530

    Add files via upload

---
+++ Day 21/Q81.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int stringLength(char str[]) {
+    int count = 0;
+
+    while (str[count] != '\0') {
+        count++;
+    }
+
+    return count;
+}
+
+int main() {
+    char str[] = "Hello";
+
+    printf("Length = %d\n", stringLength(str));
+
+    return 0;
+}
